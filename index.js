11c87d06d18c4d3e90100c3ec7df338284d372ed
kingkazuma7 <your.nasionasio7@gmail.com>
2022-12-04 14:11:13 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': "text/plain"});
+  res.end('hello wordl glitch!!\n');
+}).listen(4000);
+console.log('Server running at http://127.0.0.1:4000');
